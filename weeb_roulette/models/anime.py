25ed334e0d66671b13cd789589abf17cf2c0c310
Woojae <joki4127@gmail.com>
2022-11-17 10:37:01 -0700

    Added complex properties to Anime model

--- weeb_roulette/models/anime.py
+++ weeb_roulette/models/anime.py
@@ -1,18 +1,35 @@
 from pydantic import BaseModel
+from models.pool import pool
+
+class attributes(BaseModel):
+    synopsis: str
+
+class posterImage(BaseModel):
+    tiny : str
+    small: str
+    medium: str
+    large: str
+    original: str
+
+class coverImage(BaseModel):
+    tiny: str
+    small: str
+    large: str
+    original: str
 
 class Anime(BaseModel):
     id: str
     type: str
-    links: str #url
-    attributes: dict
+    links: str
+    attributes: attributes
     canonicalTitle: str
     startDate: str
     endDate: str
     ageRating: str
     ageRatingGuide: str
     status: str
-    posterImage: dict #url
-    coverimage: dict #url
+    posterImage: posterImage
+    coverImage: coverImage
     episodeCount: int
     episodeLength: int
     youtubeVideoId: str
@@ -20,4 +37,5 @@ class Anime(BaseModel):
 
 class AnimeList(BaseModel):
     AnimeList: list[Anime]
+
 
