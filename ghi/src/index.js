51d6b2aef39a3ac4b1286d86e83ea841fe29ea5e
Leo Galvan <toro_negro25@yahoo.com>
2022-11-21 13:23:26 -0600

    start details page

--- ghi/src/index.js
+++ ghi/src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './bloat/reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./bloat/reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
