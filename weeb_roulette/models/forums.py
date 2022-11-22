76e06a7c6a7c9bf591a2f8f888590fdedae90374
Connor <cqj.gillespie@gmail.com>
2022-11-21 18:57:39 -0600

    Forum models

--- weeb_roulette/models/forums.py
+++ weeb_roulette/models/forums.py
@@ -1,1 +1,23 @@
 from pydantic import BaseModel
+from datetime import datetime
+
+from .accounts import Account
+
+class Post(BaseModel):
+    author: Account.username
+    posted: datetime.datetime
+    edited: bool
+    likes: int
+    dislikes: int
+
+class Thread(BaseModel):
+    id: int
+    title: str
+    date_created: datetime.datetime
+    content: str
+    last_updated: datetime.datetime
+    author: Account.username
+    number_of_replies: int
+    likes: int
+    dislikes: int
+    posts: Post | None = None
