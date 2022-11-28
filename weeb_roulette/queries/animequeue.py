d987475d39b3f29e6e18f60c6a0fe8da86091d7e
Matt Sabo <synapsed@live.com>
2022-11-28 16:51:29 -0500

    added db.py

--- weeb_roulette/queries/animequeue.py
+++ weeb_roulette/queries/animequeue.py
@@ -1,43 +1,21 @@
 from bson import ObjectId
-import pymongo, os
 from models.anime import AnimeQueueIn, AnimeQueue, AnimeQueueOut
-from queries.client import Queries
+from db import database, Queries
 
-# client = pymongo.MongoClient(os.environ["DATABASE_URL"])
-# dbname = os.environ['DATABASE_NAME']
-# db = client[dbname]
-# collection = db['animequeue']
 
-# class Queries:
-#     @property
-#     def collection(self):
-#         db = client[self.DB_NAME]
-#         return db[self.COLLECTION]
-
-#     def create(data):
-#         data = dict(data)
-#         response = collection.insert_one(data)
-#         return str(response.inserted_id)
-
-#     def all():
-#         response = collection.find()
-#         data = []
-#         for i in response:
-#             i["_id"] = str(i["_id"])
-#             data.append(i)
-#         return data
+collection = database['animequeue']
 
 
 class AnimeQueueQueries(Queries):
-    DB_NAME = "anime"
+    DB_NAME = "weeb_roulette"
     COLLECTION = "animequeue"
-    
+
     def create(self, animequeue: AnimeQueueIn) -> AnimeQueueOut:
         props = animequeue.dict()
         self.collection.insert_one(props)
         props["id"] = str(props["_id"])
         return AnimeQueueOut(**props)
-    
+
     def delete(self, anime_ids: str):
         self.collection.delete_one(
             {
