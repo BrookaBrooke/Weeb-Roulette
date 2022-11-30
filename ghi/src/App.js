8e3e5750ae6ac66185a2b7aee915c37f259c22ea
Brooke Vonderheid <bevfrisco@gmail.com>
2022-11-30 12:38:28 -0500

    fixed bug

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,12 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./animeComs/mainPage";
 import AnimeList from "./animeComs/animelistPage";
-import TopNav from "./animeComs/navData/nav";
+//import TopNav from "./animeComs/navData/nav";
 
 function App() {
   return (
     <BrowserRouter>
-      <TopNav />
       <div>
         <Routes>
           <Route path="home">
