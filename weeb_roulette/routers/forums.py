6f587597788a18593cf390f3daee68d15138f776
Connor <cqj.gillespie@gmail.com>
2022-12-01 16:05:13 -0600

    Small edits

--- weeb_roulette/routers/forums.py
+++ weeb_roulette/routers/forums.py
@@ -31,29 +31,30 @@ async def create_thread(
     return thread_request
 
 
-@router.get("/threads", response_model=ThreadList)
+@router.get("/threads")
 def get_threads(repo: ThreadQueries = Depends()):
     return ThreadList(threads=repo.get_all())
 
 
-@router.get("/threads/{thread_id}", response_model=ThreadOut)
+@router.get("/thread/{id}", response_model=ThreadOut)
 def get_thread(
-    thread_id = str,
-    repo: ThreadQueries = Depends(),
-    post_repo: PostQueries = Depends(),
-    ):
-    posts = post_repo.get_by_thread(thread_id)
-    thread = repo.get(thread_id)
-    thread.posts = posts
-    return thread
+    id: str,
+    repo: ThreadQueries = Depends()
+):
+    threads = repo.get(id)
+    return threads
 
+
+@router.get("/threads/{id}")
+def get_threads_for_profile(profile_id: str, repo: ThreadQueries = Depends()):
+    return ThreadList(threads=repo.get_by_profile(profile_id))
 
 @router.post("/threads/{thread_id}/posts", response_model=PostOut)
 async def create_post(
     thread_id: str,
     post: PostIn,
     repo: PostQueries = Depends(),
-    # account: dict = Depends(get_current_user),
+    # account: dict = Depends(get_current_account_data),
 ):
     # if "user" not in account.roles:
     #     raise not_authorized
@@ -83,6 +84,30 @@ def update_thread(
     repo: ThreadQueries = Depends(),
 ):
     record = repo.update_thread(id, thread.content)
+    if record is None:
+        response.status_code = 404
+    return record
+
+@router.delete("/threads/{id}/posts", response_model=bool)
+async def delete_post(
+    id: str,
+    repo: PostQueries = Depends(),
+    # account_data: dict = Depends(authenticator.get_current_account_data),
+):
+    # account = AccountOut(**account_data)
+    # if "user" not in account.roles:
+    #     raise not_authorized
+    repo.delete_post(id=id)
+    return True
+
+@router.put("/update_post/{id}", response_model=PostOut)
+def update_post(
+    id: str,
+    post: PostOut,
+    response: Response,
+    repo: PostQueries = Depends(),
+):
+    record = repo.update_post(id, post.content)
     if record is None:
         response.status_code = 404
     return record
