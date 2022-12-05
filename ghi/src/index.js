108321bf7c0b404346ca8bbf8b8ae412139ef40d
Connor <cqj.gillespie@gmail.com>
2022-12-05 14:33:15 -0600

    Redux Store changes

--- ghi/src/index.js
+++ ghi/src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./bloat/reportWebVitals";
 import Card from "./Cads/Cards";
+import store from "./store/store";
+import { Provider } from "react-redux";
 
 // import "../node_modules/react-bootstrap/dist/css/bootstrap";
 
@@ -12,7 +14,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      < App />
+    </Provider>
   </React.StrictMode>
 );
 
