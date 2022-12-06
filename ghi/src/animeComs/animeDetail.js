3459da4fa770fa735b5db701947e3c604d420ed9
Brooke Vonderheid <bevfrisco@gmail.com>
2022-12-06 17:47:40 -0500

    connected to ap1

--- ghi/src/animeComs/animeDetail.js
+++ ghi/src/animeComs/animeDetail.js
@@ -2,24 +2,30 @@ import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 
 function AnimeDetail() {
+  const [animes, setAnimes] = useState([]);
+
+  const fetchAnimes = async () => {
+    const url = "http://localhost:8000/anime_detail/";
+    const result = await fetch(url);
+    const data = await result.json();
+    console.log(data);
+    setAnimes(data.data);
+  };
+
+  useEffect(() => {
+    fetchAnimes();
+  }, []); // componentDidMount
+
   return (
     <div>
-      <Row>
-        <Col>
-          <Card className="mb-3" style={{ color: "pink" }}>
-            <Card.Img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/MOREmoji_uwu.svg/512px-MOREmoji_uwu.svg.png"></Card.Img>
-          </Card>
-        </Col>
-
-        <Col>
-          <Card className="mb-3">
-            <Card.Body>
-              <Card.Title>yo</Card.Title>
-              <Card.Text>yo</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+      {animes.map((anime) => {
+        return (
+          <tr key={anime.id}>
+            <td className="model-title">{anime.attributes.canonicalTitle}</td>
+          </tr>
+        );
+      })}
+      <h1>title</h1>
     </div>
   );
 }
