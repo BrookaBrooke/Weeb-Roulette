b7be952ccd154ab8a91565504d8d11153aa1a01f
Brooke Vonderheid <bevfrisco@gmail.com>
2022-12-06 16:51:24 -0500

    update

--- ghi/src/animeComs/animeDetail.js
+++ ghi/src/animeComs/animeDetail.js
@@ -1,106 +1,26 @@
-// import React, { useState, useEffect } from "react";
-
-// const AnimeDetail = () => {
-//   const [animes, setAnimes] = useState([]);
-
-//   const fetchAnimes = async () => {
-//     const url = "http://localhost:8000/anime_list";
-//     const result = await fetch(url);
-//     const data = await result.json();
-//     console.log(data);
-//     setAnimes(data.data);
-//   };
-
-//   useEffect(() => {
-//     fetchAnimes();
-//   }, []); // componentDidMount
-
-//   return (
-//     <div className="container">
-//       <h1 className="header-title">List of Animes</h1>
-//       <table className="table">
-//         <thead>
-//           <tr>
-//             <th className="table-head">Name</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {animes.map((synopsis) => {
-//             return (
-//               <tr key={synopsis}>
-//                 <td className="model-text">{synopsis.attributes.synopsis}</td>
-//               </tr>
-//             );
-//           })}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// };
-// export default AnimeDetail;
-
-// BREAK
-
 import React, { useState, useEffect } from "react";
-
-// export default function AnimeDetail(props) {
-//   const [animeInfo, getAnimeInfo] = useState([]);
-//   // const { animeId } = props;
-//   // // const { data } = "http://localhost:8000/anime_list";
-
-//   // const [animeData, setAnimeData] = React.useState(null);
-//   // React.useEffect(() => {
-//   //   if (data) {
-//   //     setAnimeData(data);
-//   //   }
-//   // }, [data]);
-
-// import { useEffect, useState } from "react";
-// import AnimeList from "./animelistPage";
-// import
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 function AnimeDetail() {
-  const [animeData, setAnimeData] = useState(null);
-  const [search, setSearch] = useState("");
-  // const { animeId } = props;
-
-  useEffect(() => {
-    async function getAnimeInfo() {
-      const response = await fetch("http://localhost:8000/anime_detail");
-      const data = await response.json();
-      console.log(data);
-
-      setAnimeData(data);
-    }
-    if (animeData == null) {
-      getAnimeInfo();
-    }
-  }, []);
-
-  if (animeData === null) {
-    return <p>Loading...</p>;
-  }
   return (
-    <>
-      <table className="table table-stripped">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Detail</th>
-          </tr>
-        </thead>
-        <tbody>
-          {animeData.map((anime_data) => {
-            return (
-              <tr key={anime_data.title}>
-                <td> {anime_data.title} </td>
-                <td> {anime_data} </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </>
+    <div>
+      <Row>
+        <Col>
+          <Card className="mb-3" style={{ color: "pink" }}>
+            <Card.Img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/MOREmoji_uwu.svg/512px-MOREmoji_uwu.svg.png"></Card.Img>
+          </Card>
+        </Col>
+
+        <Col>
+          <Card className="mb-3">
+            <Card.Body>
+              <Card.Title>yo</Card.Title>
+              <Card.Text>yo</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </div>
   );
 }
 
