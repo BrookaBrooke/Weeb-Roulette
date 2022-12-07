3fb6591ec0d1fc5237ca7db7a0e828c5d9c4bd9c
Woojae <joki4127@gmail.com>
2022-12-07 11:52:33 -0700

    Working redux store

--- ghi/src/store/store.js
+++ ghi/src/store/store.js
@@ -1,15 +1,30 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import { setupListeners } from '@reduxjs/toolkit/query';
+// import { accountSlice } from './accountSlice';
+
+// export const store = configureStore({
+//     reducer: {
+//         [accountSlice.reducerPath]: accountSlice.reducer,
+//     },
+//     middleware: getDefaultMiddleware => {
+//         return getDefaultMiddleware()
+//             .concat(accountSlice.middleware)
+//     }
+// })
+
+// setupListeners(store.dispatch)
+
 import { configureStore } from '@reduxjs/toolkit';
-import { setupListeners } from '@reduxjs/toolkit/query';
-import { accountSlice } from './accountSlice';
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { animeApi } from './animeApi';
 
 export const store = configureStore({
-    reducer: {
-        [accountSlice.reducerPath]: accountSlice.reducer,
-    },
-    middleware: getDefaultMiddleware => {
-        return getDefaultMiddleware()
-            .concat(accountSlice.middleware)
-    }
+  reducer: {
+    [animeApi.reducerPath] : animeApi.reducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(animeApi.middleware),
 })
 
 setupListeners(store.dispatch)
+
