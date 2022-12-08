99292f50c5f3f819ec0725bfc74eeba82df7c394
Brooke Vonderheid <bevfrisco@gmail.com>
2022-12-08 12:00:00 -0500

    shortened code

--- ghi/src/animeComs/animeDetail.js
+++ ghi/src/animeComs/animeDetail.js
@@ -1,70 +1,29 @@
 import { Container, Row, Col, Card } from "react-bootstrap";
 import React, { useState, useEffect } from "react";
 
-const AnimeDetail = (item) => {
-  // const { data } = useGetAnimeQuery();
-  const [animes, setAnimes] = useState([]);
-  // const [detail, setDetail] = useState([]);
-  // const [addtoqueue, setQueue] = useState([])
-  const [currentPage, setCurrentPage] = useState();
+import { useGetAnimeQuery } from "../store/animeApi";
 
-  const fetchAnimeDetail = async (id) => {
-    const url = `http://localhost:8000/anime_detail/${id}`;
-    const result = await fetch(url);
-    const data = await result.json();
-    setAnimes(data.data);
-  };
+function AnimeDetail() {
+  const { data, error, isLoading } = useGetAnimeQuery();
 
-  useEffect(() => {
-    fetchAnimeDetail();
-  }, []); // componentDidMount
-
-  function Card(props) {
-    return (
-      <div className="card">
-        <div className="card_body">
-          <a href="http://localhost:3000/detail/{id}" role="button">
-            <img src={props.img} alt="" />
-          </a>
-          <h2 className="card_title">{props.title}</h2>
-          <p className="card_description">{props.description}</p>
-        </div>
-      </div>
-    );
+  if (isLoading) {
+    return <progress>"progrss is primary" max="100" </progress>;
   }
 
   return (
     <div className="container">
-      <h1 className="header-title">detail</h1>
-      <div className="extra container">
-        <table className="table">
-          <thead>
-            <tr>
-              <th className="table-head"></th>
+      <h1 className="header-title">Detail</h1>
+      <table className="table">
+        {data.data.map((anime) => {
+          return (
+            <tr key={anime.id}>
+              <td className="model-text"></td>
             </tr>
-          </thead>
-          <tbody>
-            {animes.map((anime) => {
-              return (
-                <tr key={anime.id}>
-                  <td className="model-text">
-                    <Card
-                      img={anime.attributes.posterImage.tiny}
-                      title={anime.attributes.canonicalTitle}
-                      synopsis={anime.attributes.synopsis}
-                      episodecount={anime.attributes.episodeCount}
-                    />
-                  </td>
-                  {/* Figure out on click redirect to detail page */}
-                  {/* assign id value to onclick function */}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+          );
+        })}
+      </table>
     </div>
   );
-};
+}
 
 export default AnimeDetail;
