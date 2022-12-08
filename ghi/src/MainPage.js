df0a2be648dc8277a8d94b2c0944383354fa6855
Leo Galvan <toro_negro25@yahoo.com>
2022-12-08 15:16:19 -0600

    cont working on front page

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -1,9 +1,74 @@
+import React, { useEffect, useState } from "react";
+import AnimeCard from "./components/AnimeCard";
+import Container from "react-bootstrap/Container";
+import Carousel from "react-bootstrap/Carousel";
 
+const MainPage = () => {
+  const [animes, setAnimes] = useState([]);
+  const [homepageCards, setHomepageCards] = useState([]);
 
-function MainPage() {
-    return (
-        <p>Test</p>
-    );
-}
+  const fetchAnimes = async () => {
+    const url = "http://localhost:8000/anime_list/1";
+    const result = await fetch(url);
+    const data = await result.json();
+    setAnimes(data.data);
+    const array = [];
+    let slideObject = {};
+    let index = 0;
+    data.data.map((cards, i) => {
+      slideObject[index] = cards;
+      if (i % 4 === 0) {
+        array.push(slideObject);
+        slideObject = {};
+        index = 0;
+      }
+      index++;
+    });
+    array.shift();
+    setHomepageCards(array);
+  };
 
+  useEffect(() => {
+    fetchAnimes();
+  }, []);
+
+  return (
+    <Container className="card-carousel">
+      <Carousel style={{ width: "100%" }}>
+        {homepageCards.map((i) => {
+          return (
+            <Carousel.Item key={i[1].id}>
+              <div style={{ display: "flex" }}>
+                <span className="m-2" style={{ width: "100%" }}>
+                  <AnimeCard
+                    title={i[1].attributes.canonicalTitle}
+                    image={i[1].attributes.posterImage?.tiny}
+                  />
+                </span>
+                <span className="m-2" style={{ width: "100%" }}>
+                  <AnimeCard
+                    title={i[2].attributes.canonicalTitle}
+                    image={i[2].attributes.posterImage?.tiny}
+                  />
+                </span>
+                <span className="m-2" style={{ width: "100%" }}>
+                  <AnimeCard
+                    title={i[3].attributes.canonicalTitle}
+                    image={i[3].attributes.posterImage?.tiny}
+                  />
+                </span>
+                <span className="m-2" style={{ width: "100%" }}>
+                  <AnimeCard
+                    title={i[4].attributes.canonicalTitle}
+                    image={i[4].attributes.posterImage?.tiny}
+                  />
+                </span>
+              </div>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+    </Container>
+  );
+};
 export default MainPage;
