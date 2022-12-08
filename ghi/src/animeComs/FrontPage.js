2fa452ce2ffd7e9a8231d20f60ba3b96dd3cb753
Leo Galvan <toro_negro25@yahoo.com>
2022-12-08 13:58:32 -0600

    complete carousel

--- ghi/src/animeComs/FrontPage.js
+++ ghi/src/animeComs/FrontPage.js
@@ -4,20 +4,19 @@ import Container from "react-bootstrap/Container";
 import Carousel from "react-bootstrap/Carousel";
 
 const FrontPage = () => {
-  const [animes, setHomepageCards] = useState([]);
-  // const [homepageCards, setHomepageCards] = useState([]);
+  const [animes, setAnimes] = useState([]);
+  const [homepageCards, setHomepageCards] = useState([]);
 
   const fetchAnimes = async () => {
     const url = "http://localhost:8000/anime_list/1";
     const result = await fetch(url);
     const data = await result.json();
-    console.log(data);
-    setHomepageCards(data.data);
+    setAnimes(data.data);
     const array = [];
     let slideObject = {};
     let index = 0;
-    animes.cards.forEach((animes, i) => {
-      slideObject[index] = animes;
+    data.data.map((cards, i) => {
+      slideObject[index] = cards;
       if (i % 4 === 0) {
         array.push(slideObject);
         slideObject = {};
@@ -35,42 +34,34 @@ const FrontPage = () => {
 
   return (
     <Container className="card-carousel">
-      <Carousel style={{ width: "290px" }}>
-        {animes.map((i) => {
+      <Carousel style={{ width: "100%" }}>
+        {homepageCards.map((i) => {
           return (
-            <Carousel.Item key={i.id["1"]}>
+            <Carousel.Item key={i[1].id}>
               <div style={{ display: "flex" }}>
                 <span className="m-2" style={{ width: "100%" }}>
-                  <Link to={`/card/${cardObject["1"].multiverse_id}`}>
-                    <AnimeCard
-                      title={i.attributes.canonicalTitle}
-                      images={i.attributes.posterImages?.tiny}
-                    />
-                  </Link>
+                  <AnimeCard
+                    title={i[1].attributes.canonicalTitle}
+                    image={i[1].attributes.posterImage?.tiny}
+                  />
                 </span>
                 <span className="m-2" style={{ width: "100%" }}>
-                  <Link to={`/card/${cardObject["2"].multiverse_id}`}>
-                    <AnimeCard
-                      title={i.attributes.canonicalTitle}
-                      images={i.attributes.posterImage?.tiny}
-                    />
-                  </Link>
+                  <AnimeCard
+                    title={i[2].attributes.canonicalTitle}
+                    image={i[2].attributes.posterImage?.tiny}
+                  />
                 </span>
                 <span className="m-2" style={{ width: "100%" }}>
-                  <Link to={`/card/${cardObject["3"].multiverse_id}`}>
-                    <AnimeCard
-                      title={i.attributes.canonicalTitle}
-                      images={i.attributes.posterImage?.tiny}
-                    />
-                  </Link>
+                  <AnimeCard
+                    title={i[3].attributes.canonicalTitle}
+                    image={i[3].attributes.posterImage?.tiny}
+                  />
                 </span>
                 <span className="m-2" style={{ width: "100%" }}>
-                  <Link to={`/card/${cardObject["4"].multiverse_id}`}>
-                    <AnimeCard
-                      title={i.attributes.canonicalTitle}
-                      images={i.attributes.posterImage?.tiny}
-                    />
-                  </Link>
+                  <AnimeCard
+                    title={i[4].attributes.canonicalTitle}
+                    image={i[4].attributes.posterImage?.tiny}
+                  />
                 </span>
               </div>
             </Carousel.Item>
@@ -81,61 +72,3 @@ const FrontPage = () => {
   );
 };
 export default FrontPage;
-
-// useEffect(() => {
-//   fetchAnimes();
-// }, []); // componentDidMount
-
-// return (
-//   <div className="container">
-//     <div
-//       id="carouselExampleIndicators"
-//       className="carousel slide"
-//       data-bs-ride="carousel"
-//     >
-//       {animes.map((i) => {
-//         console.log("hello", i.id);
-
-//           return (
-//             <>
-//               <div className="carousel-inner">
-//                 <div className="carousel-item active">
-//                   <AnimeCard
-//                     key={i.id}
-//                     title={i.attributes.canonicalTitle}
-//                     // description={i.attributes.description}
-//                     image={i.attributes.posterImage?.small}
-//                   />
-//                 </div>
-//               </div>
-//               <button
-//                 className="carousel-control-prev"
-//                 type="button"
-//                 data-bs-target="#carouselExampleControls"
-//                 data-bs-slide="prev"
-//               >
-//                 <span
-//                   className="carousel-control-prev-icon"
-//                   aria-hidden="true"
-//                 />
-//                 <span className="visually-hidden">Previous</span>
-//               </button>
-//               <button
-//                 className="carousel-control-next"
-//                 type="button"
-//                 data-bs-target="#carouselExampleControls"
-//                 data-bs-slide="next"
-//               >
-//                 <span
-//                   className="carousel-control-next-icon"
-//                   aria-hidden="true"
-//                 />
-//                 <span className="visually-hidden">Next</span>
-//               </button>
-//             </>
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// };
--- ghi/src/components/AnimeCard.js
+++ ghi/src/components/AnimeCard.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const AnimeCard = (props) => {
   return (
-    <div className="carousel-item" style={{ width: "18rem" }}>
-      <img src={props.image} className="d-block w-100" />
+    <div className="card" style={{ width: "18rem" }}>
+      <img src={props.image} alt="" />
       <div className="card-body">
-        <h5 className="card-title">{props.title}</h5>
+        <h3 className="card-title">{props.title}</h3>
         {/* <p className="card-text">{props.description}</p> */}
 
-        <a href="" className="btn btn-primary">
+        {/* <a href="" className="btn btn-primary">
           Go somewhere
-        </a>
+        </a> */}
       </div>
     </div>
   );
