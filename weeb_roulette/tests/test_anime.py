9114d36d9bce5aa9800e3f40a24ed8c5eac68eb7
Woojae <joki4127@gmail.com>
2022-12-12 08:50:16 -0700

    Made test and finished ReadMe

--- weeb_roulette/tests/test_anime.py
+++ weeb_roulette/tests/test_anime.py
@@ -5,4 +5,22 @@ from queries.anime import AnimeQueueQueries
 client = TestClient(app)
 
 
-# class MockAnimeRepository:
+class MockAnimeRepository:
+    def get_queues(self):
+        return []
+
+
+def test_get_anime_queues():
+
+    # Arrange
+    app.dependency_overrides[AnimeQueueQueries] = MockAnimeRepository
+
+    # Act
+    response = client.get("/anime_queues")
+
+    # Assert
+    assert response.status_code == 200
+    assert response.json() == {'animequeues': []}
+
+    # Clean up
+    app.dependency_overrides = {}
